9486be0176c9523fe1a8f637406a6ba96c6c9560
samyoga <bhattaraisamyoga@gmail.com>
2019-07-24 22:09:57 +0545

    Realm database setup and initialization

--- app/src/main/java/com/example/hamrovidhyalaya/MainActivity.java
+++ app/src/main/java/com/example/hamrovidhyalaya/MainActivity.java
@@ -4,11 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Realm.init(this);
+        RealmConfiguration realmConfig = new RealmConfiguration.Builder()
+                .name("login.realm")
+                .schemaVersion(0)
+                .build();
+        Realm.setDefaultConfiguration(realmConfig);
     }
 }
