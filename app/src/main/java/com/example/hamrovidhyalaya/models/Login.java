bac674c4bdea9ee28e0e317baa62be095af75b64
samyoga <bhattaraisamyoga@gmail.com>
2019-07-24 22:22:14 +0545

    model class login modified with getter and setter methods

--- app/src/main/java/com/example/hamrovidhyalaya/models/Login.java
+++ app/src/main/java/com/example/hamrovidhyalaya/models/Login.java
@@ -1,4 +1,41 @@
 package com.example.hamrovidhyalaya.models;
 
-public class Login {
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+import io.realm.annotations.Required;
+
+public class Login extends RealmObject {
+
+    @Required
+    @PrimaryKey
+    private String id;
+
+    @Required
+    private String username;
+    private String password;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
 }
